aea49375576c23584630ffaaab66b669384d90f9
ERIN CARTER <carter.erin86@gmail.com>
2018-11-28 10:41:26 -0500

    typos uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
-	config.fog_credentials = {
-		:provider              => 'AWS',
+  config.fog_credentials = {
+    :provider              => 'AWS',
     :aws_access_key_id     => "#{ENV['AWS_ACCESS_KEY_ID']}",
     :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
     :region                => "#{ENV['S3_REGION']}",
@@ -10,5 +10,4 @@ CarrierWave.configure do |config|
 
   config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
   config.cache_dir     = "#{Rails.root}/tmp/uploads"
-	
 end
